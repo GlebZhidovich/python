62a31f87a30fe3784a39fa2454f1731315e35619
GlebZhidovich <gzhidovich@gmail.com>
2020-11-26 23:08:40 +0300

    feat: add async request

---
+++ async_request.py
@@ -0,0 +1,8 @@
+import aiohttp
+
+async def ip_request():
+    url = "http://jsonplaceholder.typicode.com/posts"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            response_json = await response.json()
+            return response_json[0]
